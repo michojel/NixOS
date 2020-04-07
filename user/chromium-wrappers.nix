d96135bcccf1face9ffa8b2a338dd90c7cb0cb43
Michal Minář <mic.liamg@gmail.com>
2020-04-07 08:37:27 +0200

    chromium-wrappers: create desktop files
    added few icons
    
    Signed-off-by: Michal Minář <mic.liamg@gmail.com>

--- user/chromium-wrappers.nix
+++ user/chromium-wrappers.nix
@@ -5,117 +5,158 @@ let
   dataDirBase = "/home/miminar/.config/chromium";
   workProfile = "RedHat";
   defaultWMClass = "Chromium";
+
+  mkWMClass = { profile ? null, appId ? null }: (
+    "${defaultWMClass}" + (lib.optionalString (profile != null) ("." + lib.toLower profile))
+    + (lib.optionalString (appId != null) (".crx_" + lib.toLower appId))
+  );
+
+  mkDesktopItem =
+    { name
+    , profile ? null
+    , appId ? null
+    , icon ? null
+    , longName ? null
+    , description ? null
+    , comment ? null
+    , categories ? null
+    , mimeTypes ? null
+    }: (
+      makeDesktopItem {
+        name = name;
+        desktopName = longName;
+        genericName = description;
+        icon = icon;
+        exec = name;
+        categories = if (categories != null) then lib.concatStringsSep ";" categories else "Network;WebBrowser";
+        mimeType = lib.optionalString (mimeTypes != null) (lib.concatStringsSep ";" mimeTypes);
+        startupNotify = "true";
+        extraEntries = "StartupWMClass=" + mkWMClass { inherit profile appId; };
+      }
+    );
+
+  mkWrapper =
+    { name
+    , profile ? null
+    , appId ? null
+    , icon ? null
+    , longName ? null
+    , description ? null
+    , comment ? null
+    , categories ? null
+    , mimeTypes ? null
+    }:
+      let
+        desktopItem = mkDesktopItem {
+          inherit name profile appId icon longName description comment categories mimeTypes;
+        };
+      in
+        lib.concatStringsSep "\n" (
+          [
+            (
+              lib.concatStringsSep
+                " "
+                (
+                  [
+                    "makeWrapper"
+                    "${chromium}/bin/chromium"
+                    "$out/bin/${name}"
+                    "--add-flags"
+                    ("--user-data-dir=" + dataDirBase + (lib.optionalString (profile != null) ("." + lib.toLower profile)))
+                    "--add-flags"
+                    ("--class=" + mkWMClass { inherit profile appId; })
+                  ] ++ (
+                    if (appId != null) then
+                      [ "--add-flags" "--app-id=${appId}" ]
+                    else []
+                  ) ++ (
+                    if (profile == workProfile) then
+                      [ "--add-flags" ''--auth-server-whitelist="*.redhat.com"'' ]
+                    else []
+                  )
+                )
+            )
+
+            ''
+              pushd ${desktopItem}
+                find "share/applications" -type f | xargs -i ln -v -s "${desktopItem}/{}" "$out/{}"
+              popd
+            ''
+
+          ] ++ (
+            if (icon != null) then (
+              let
+                pngname = lib.replaceStrings [ ".svg" ] [ ".png" ] icon;
+              in
+                [
+                  ''convert +antialias -background transparent -verbose \
+                      -size 128x128 "chromium-wrappers/${icon}" "$out/share/icons/${pngname}"''
+                  ''
+                    for size in 16x16 24x24 32x32 48x48 64x64 72x72 96x96 128x128 192x192 256x256 512x512 1024x1024; do
+                      mkdir -p $out/share/icons/hicolor/$size/apps
+                      convert +antialias -background transparent -verbose \
+                          -size $size "chromium-wrappers/${icon}" "$out/share/icons/hicolor/$size/apps/${pngname}"
+                    done
+                  ''
+                ]
+            ) else []
+          )
+        );
+
+  wrappers = [
+    (mkWrapper { name = "chromium-private"; longName = "Personal Chromium Browser"; icon = "redhat.svg"; })
+    (mkWrapper { name = "gcalendar"; longName = "Personal Google Calendar"; appId = "kjbdgfilnfhdoflbpgamdcdgpehopbep"; icon = "Google_Calendar_icon.svg"; })
+    (mkWrapper { name = "gcontacts"; longName = "Personal Google Contacts"; appId = "jbeoliebicnmljhmdbbdeljdpjbfollk"; })
+    (mkWrapper { name = "gdocs"; longName = "Personal Google Docs"; appId = "bojccfnmcnekjgjhcaklmcgofnngpjcl"; })
+    (mkWrapper { name = "gsheets"; longName = "Personal Google Sheets"; appId = "lcahnhkcfaikkapifpaenbabamhfnecc"; })
+    (mkWrapper { name = "gdrive"; longName = "Personal Google Drive"; appId = "lkdnjjllhbbhgjfojnheoooeabjimbka"; })
+    (mkWrapper { name = "gravit"; longName = "Gravit Online"; appId = "pdagghjnpkeagmlbilmjmclfhjeaapaa"; })
+    (mkWrapper { name = "duolingo"; longName = "Duolingo"; appId = "aiahmijlpehemcpleichkcokhegllfjl"; })
+    (mkWrapper { name = "gmail"; longName = "Personal Google Mail"; appId = "pjkljhegncpnkpknbcohdijeoejaedia"; icon = "Gmail_Icon.svg"; })
+    (mkWrapper { name = "gmaps"; longName = "Google Maps"; appId = "lneaknkopdijkpnocmklfnjbeapigfbh"; })
+    (mkWrapper { name = "mapy"; longName = "Seznam Mapy"; appId = "mnadlckdoclecdmddabnbgjnkfoiddpd"; })
+    (mkWrapper { name = "mega"; longName = "Mega in Chromium"; appId = "ockmlcfhhimcljikencdeppnoljjjfjk"; })
+    (mkWrapper { name = "gmessages"; longName = "Personal Google Messages"; appId = "hpfldicfbfomlpcikngkocigghgafkph"; })
+    (mkWrapper { name = "gphotos"; longName = "Google Photos"; appId = "blckliiiahkijfikcfmbncibcefakemp"; })
+    (mkWrapper { name = "skypeweb"; longName = "Skype on Web"; appId = "bjdilgfelnbljgdpngladebaeggachpa"; })
+    (mkWrapper { name = "whatsapp"; longName = "Whatsapp"; appId = "hnpfjngllnobngcgfapefoaidbinmjnm"; })
+    #(mkWrapper { name = "wikics"; appId = "enjdmlmicjdnokcbaeajgmnippjnkfmo"; })
+    #(mkWrapper { name = "wikide"; appId = "bhdbngpdfcdnndblpfphbmkajcbpnean"; })
+    #(mkWrapper { name = "wikien"; appId = "mopbmgngnfadcehgbmkgjblgbhiehmlm"; })
+    #(mkWrapper { name = "wikiru"; appId = "oenmclfdgkfbfladdhglinfmbbgnljhj"; })
+    (mkWrapper { name = "webflow"; longName = "Webflow"; appId = "fjjpcpdfpiaiifjpdjeilpjolhkcdpne"; })
+    (mkWrapper { name = "wireweb"; longName = "Wire on Web"; appId = "kfhkficiiapojlgcnbkgacfjmpffgoki"; })
+    (mkWrapper { name = "youtube"; longName = "Youtube"; appId = "blpcfgokakmgnkcojhhkbfbldkacnbeo"; icon = "YouTube_social_white_squircle_(2017).svg"; })
+    (mkWrapper { name = "ytmusic"; longName = "Youtube Music"; appId = "cinhimbnkkaeohfgghhklpknlkffjgod"; icon = "Youtube_Music_logo.svg"; })
+
+    (mkWrapper { name = "chromium-work"; longName = "RHT Chromium Browser"; })
+    (mkWrapper { name = "ibmbox"; longName = "IBM Box"; profile = workProfile; appId = "gckfeldgkmajgieiakjfpmoahpajonjg"; })
+    (mkWrapper { name = "rhbj"; longName = "RHT Bluejeans"; profile = workProfile; appId = "mncjkohjkaeaoabfmhdefaflkcjjkmdd"; })
+    (mkWrapper { name = "rhgcalendar"; longName = "RHT Calendar"; profile = workProfile; appId = "kjbdgfilnfhdoflbpgamdcdgpehopbep"; })
+    (mkWrapper { name = "rhgchat"; longName = "RHT Google Chat"; profile = workProfile; appId = "pommaclcbfghclhalboakcipcmmndhcj"; })
+    (mkWrapper { name = "rhgmessages"; longName = "RHT Messages"; profile = workProfile; appId = "kpbdgbekoclglmjckpbanehbpjnlphkf"; })
+    (mkWrapper { name = "rhgdocs"; longName = "RHT Google Docs"; profile = workProfile; appId = "gcefppfnjnmndpknenooeofkfcbakpkp"; })
+    (mkWrapper { name = "rhgdrive"; longName = "RHT Google Drive"; profile = workProfile; appId = "lkdnjjllhbbhgjfojnheoooeabjimbka"; })
+    (mkWrapper { name = "rhgmail"; longName = "RHT Google Mail"; profile = workProfile; appId = "nkcknjnfmnmjahcahhhjgakeikoiomof"; })
+    (mkWrapper { name = "rhgsheets"; longName = "RHT Google Sheets"; profile = workProfile; appId = "albjknpbljlpmmpfjicdohagjcifagdi"; })
+    (mkWrapper { name = "sapcalendar"; longName = "SAP Calendar"; profile = workProfile; appId = "oeogacjkgmanlfjadbnhngnpbkibgfhj"; })
+    (mkWrapper { name = "sapdrive"; longName = "SAP Drive"; profile = workProfile; appId = "phgkmbchjgnehfpnmbekcoclneeojdma"; })
+    (mkWrapper { name = "sapmail"; longName = "SAP Mail"; profile = workProfile; appId = "plnbadkpncgbnekpephdpooeafambhak"; })
+    (mkWrapper { name = "sapteams"; longName = "SAP Teams"; profile = workProfile; appId = "jofcjnlbhnljdeapdjgodjlakohpfnjo"; })
+  ];
 in
 stdenv.mkDerivation {
   name = "chromium-wrappers";
   version = chromium.version;
   meta = chromium.meta;
-  nativeBuildInputs = [ makeWrapper chromium ];
+  nativeBuildInputs = [ makeWrapper chromium imagemagick ];
   runtimeDependencies = [ chromium kerberos ];
-  phases = [ "installPhase" ];
+  phases = [ "unpackPhase" "installPhase" ];
+  srcs = [
+    ./pics/chromium-wrappers
+  ];
+  sourceRoot = ".";
   installPhase = ''
-    function wrapChromiumProfile() {
-      # Positional Arguments:
-      #  1st - wrapper name
-      #  2nd - user data direcotory path
-      #  3rd - (optional) WM class name suffix
-      #  ... - (optional) any other flag will be treated as a general flag
-      local wrappername="$1"
-      local args=( --add-flags "--user-data-dir=$2")
-      if [[ "$#" -gt 2 && -n "''${3:-}" ]]; then
-        args+=( --add-flags "--class=${defaultWMClass}.$3" )
-        shift
-      fi
-      while [[ "$#" -gt 2 ]]; do
-        args+=( --add-flags "$3" )
-        shift
-      done
-      makeWrapper "${chromium}/bin/chromium" "$out/bin/''${wrappername}" "''${args[@]}"
-    }
-    function wrapChromium() {
-      # Positional Arguments:
-      #  1st - wrapper name
-      #  2nd - (optional) chrome application id
-      local wrappername="$1"
-      shift
-      local args=( "''${wrappername}" "${dataDirBase}" "" )
-      if [[ "$#" -gt 0 ]]; then
-        args+=( "--app-id=$1" )
-        shift
-      fi
-      wrapChromiumProfile "''${args[@]}"
-    }
-    function wrapChromiumRH() {
-      # Positional Arguments:
-      #  1st - wrapper name
-      #  2nd - (optional) chrome application id
-      local userdatadir="${dataDirBase}-${workProfile}"
-      local wrappername="$1"
-      shift
-      local args=( "''${wrappername}" "''${userdatadir,,}" "redhat" )
-      if [[ "$#" -gt 0 ]]; then
-        args+=( "--app-id=$1" )
-        shift
-      fi
-      # '--host-resolver-rules="MAP * ~NOTFOUND , EXCLUDE 127.0.0.1"'
-      args+=(
-        '--auth-server-whitelist="*.redhat.com"'
-      )
-      wrapChromiumProfile "''${args[@]}"
-    }
-
-    wrapChromium chromium-private
-    wrapChromium gcalendar kjbdgfilnfhdoflbpgamdcdgpehopbep
-    wrapChromium gcontacts jbeoliebicnmljhmdbbdeljdpjbfollk
-    wrapChromium gdocs     bojccfnmcnekjgjhcaklmcgofnngpjcl
-    wrapChromium gsheets   lcahnhkcfaikkapifpaenbabamhfnecc
-    wrapChromium gdrive    lkdnjjllhbbhgjfojnheoooeabjimbka
-    wrapChromium gravit    pdagghjnpkeagmlbilmjmclfhjeaapaa
-    wrapChromium duolingo  aiahmijlpehemcpleichkcokhegllfjl
-    wrapChromium gmail     pjkljhegncpnkpknbcohdijeoejaedia
-    wrapChromium gmaps     lneaknkopdijkpnocmklfnjbeapigfbh
-    wrapChromium mapy      mnadlckdoclecdmddabnbgjnkfoiddpd
-    wrapChromium mega      ockmlcfhhimcljikencdeppnoljjjfjk
-    wrapChromium gmessages hpfldicfbfomlpcikngkocigghgafkph
-    wrapChromium gphotos   blckliiiahkijfikcfmbncibcefakemp
-    wrapChromium skypeweb  bjdilgfelnbljgdpngladebaeggachpa
-    wrapChromium whatsapp  hnpfjngllnobngcgfapefoaidbinmjnm
-
-    wrapChromium wikics    enjdmlmicjdnokcbaeajgmnippjnkfmo
-    wrapChromium wikide    bhdbngpdfcdnndblpfphbmkajcbpnean
-    wrapChromium wikien    mopbmgngnfadcehgbmkgjblgbhiehmlm
-    wrapChromium wikiru    oenmclfdgkfbfladdhglinfmbbgnljhj
-
-    wrapChromium webflow   fjjpcpdfpiaiifjpdjeilpjolhkcdpne
-    wrapChromium wireweb   kfhkficiiapojlgcnbkgacfjmpffgoki
-    wrapChromium youtube   blpcfgokakmgnkcojhhkbfbldkacnbeo
-    wrapChromium ytmusic   cinhimbnkkaeohfgghhklpknlkffjgod
-
-    userdatadir="${dataDirBase}-${workProfile}"
-    wrapChromiumRH chromium-work
-    wrapChromiumRH ibmbox      gckfeldgkmajgieiakjfpmoahpajonjg
-    wrapChromiumRH rhbj        mncjkohjkaeaoabfmhdefaflkcjjkmdd
-    wrapChromiumRH rhgcalendar kjbdgfilnfhdoflbpgamdcdgpehopbep
-    wrapChromiumRH rhgchat     pommaclcbfghclhalboakcipcmmndhcj
-    wrapChromiumRH rhgmessages kpbdgbekoclglmjckpbanehbpjnlphkf
-    wrapChromiumRH rhgdocs     gcefppfnjnmndpknenooeofkfcbakpkp
-    wrapChromiumRH rhgdrive    lkdnjjllhbbhgjfojnheoooeabjimbka
-    wrapChromiumRH rhgmail     nkcknjnfmnmjahcahhhjgakeikoiomof
-    wrapChromiumRH rhgsheets   albjknpbljlpmmpfjicdohagjcifagdi
-    wrapChromiumRH sapcalendar oeogacjkgmanlfjadbnhngnpbkibgfhj
-    wrapChromiumRH sapdrive    phgkmbchjgnehfpnmbekcoclneeojdma
-    wrapChromiumRH sapmail     plnbadkpncgbnekpephdpooeafambhak
-    wrapChromiumRH sapteams    jofcjnlbhnljdeapdjgodjlakohpfnjo
-
-    # TODO: create desktop shortcuts
-    #  #!/nix/store/r8x2fx08nkka2n6ikrlnfz2z8r0b6gb0-xdg-utils-1.1.2/bin/xdg-open
-    #  [Desktop Entry]
-    #  Version=1.0
-    #  Terminal=false
-    #  Type=Application
-    #  Name=Wire
-    #  Exec=/nix/store/iw0bbvfn8w12a1mfpl878faylz3girm5-chromium-71.0.3578.98/libexec/chromium/chromium --user-data-dir=/home/miminar/.config/chromium --profile-directory=Default --app-id=kfhkficiiapojlgcnbkgacfjmpffgoki
-    #  Icon=chrome-kfhkficiiapojlgcnbkgacfjmpffgoki-Default
-    #  StartupWMClass=crx_kfhkficiiapojlgcnbkgacfjmpffgoki
-  '';
+    mkdir -p $out/share/applications $out/share/icons
+  '' + lib.concatStringsSep "\n" wrappers;
 }
---
+++ user/pics/chromium-wrappers/README.md
@@ -0,0 +1,14 @@
+## Sources
+
+- Chromium_Logo.svg
+  - https://de.wikipedia.org/wiki/Datei:Chromium_Logo.svg
+- Chrome_Logo.svg
+  - https://en.wikipedia.org/wiki/File:Chrome_Logo.svg
+- Gmail_Icon.svg
+  - https://commons.wikimedia.org/wiki/File:Gmail_Icon.svg
+- Google_Calendar_icon.svg
+  - https://de.wikipedia.org/wiki/Datei:Google_Calendar_icon.svg
+- YouTube_social_white_squircle_(2017).svg
+  - https://commons.wikimedia.org/wiki/File:YouTube_social_white_squircle_(2017).svg
+- Youtube_Music_logo.svg
+  - https://commons.wikimedia.org/wiki/File:Youtube_Music_logo.svgPaste>
