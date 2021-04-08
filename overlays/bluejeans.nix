dc9735c53517f1a901461963dde47b73a077b000
Michal Minář <mic.liamg@gmail.com>
2021-04-08 09:54:31 +0200

    bluejeans-gui: bumped to 2.21.3.2
    Signed-off-by: Michal Minář <mic.liamg@gmail.com>

--- overlays/bluejeans.nix
+++ overlays/bluejeans.nix
@@ -2,15 +2,15 @@ self: super: {
   bluejeans-gui = super.bluejeans-gui.overrideDerivation (
     oldAttrs:
     let
-      version = "2.20.0";
-      revision = "118";
+      version = "2.21.3";
+      revision = "2";
     in
     {
       version = version;
       name = "bluejeans-gui-${version}";
       src = super.fetchurl {
         url = "https://swdl.bluejeans.com/desktop-app/linux/${version}/BlueJeans_${version}.${revision}.rpm";
-        sha256 = "0bi4wmqsdj74ifgwjq1jvzccm3x9ixqja7i091qpzgds132gaabq";
+        sha256 = "006mlcrxmcgldds3vx03aghvpnh12i40jxxkdg5n4ria36xl9x3b";
       };
     }
   );
