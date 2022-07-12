2d8101710b1544e2d46482364dc743c7124e632d
Michal Minář <mm@michojel.cz>
2022-07-12 12:11:34 +0200

    mint15: enabled virtualbox
    Signed-off-by: Michal Minář <mm@michojel.cz>

--- mint15/configuration.nix
+++ mint15/configuration.nix
@@ -105,10 +105,10 @@ in
   security.pki.certificates = import /mnt/nixos/secrets/certs/certs.nix;
 
   virtualisation = {
-    #    virtualbox.host = {
-    #      enable = true;
-    #      enableExtensionPack = true;
-    #    };
+    virtualbox.host = {
+      enable = true;
+      enableExtensionPack = true;
+    };
   };
 
   sound.enable = true;
