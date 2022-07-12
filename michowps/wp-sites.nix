f4bacbba70c6476a63b6053174342b69273161e5
Michal Minář <mic.liamg@gmail.com>
2022-07-12 15:11:13 +0200

    michowps: wp: updated version
    Signed-off-by: Michal Minář <mic.liamg@gmail.com>

--- michowps/wp-sites.nix
+++ michowps/wp-sites.nix
@@ -27,8 +27,8 @@
         name = "akismet-plugin";
         # Download the theme from the wordpress site
         src = pkgs.fetchurl {
-          url = https://downloads.wordpress.org/plugin/akismet.4.1.10.zip;
-          sha256 = "0d6m2h04x2pjpz4bnxcbb7mv3b221p2hsmys6r3jcpgbil025hfj";
+          url = https://downloads.wordpress.org/plugin/akismet.4.2.5.zip;
+          sha256 = "sha256-HFRyJ01n61Bfd3bT6QbEZtqirJEG0mXZV7Q0SNP3Rik=";
         };
         # We need unzip to build this package
         buildInputs = [ pkgs.unzip ];
@@ -40,8 +40,8 @@
         name = "modula-plugin";
         # Download the theme from the wordpress site
         src = pkgs.fetchurl {
-          url = https://downloads.wordpress.org/plugin/modula-best-grid-gallery.2.5.3.zip;
-          sha256 = "199qwv2k1d62n8qqk47irb2jcfys3vpz87xlmklpfmabh1hwgwf9";
+          url = https://downloads.wordpress.org/plugin/modula-best-grid-gallery.2.6.7.zip;
+          sha256 = "0nci3jx320bxq2chp2d78x03mk5ih86k7qxiv56rqzzhv1b3yqpj";
         };
         # We need unzip to build this package
         buildInputs = [ pkgs.unzip ];
@@ -80,8 +80,8 @@
         name = "bravada-theme";
         # Download the theme from the wordpress site
         src = pkgs.fetchurl {
-          url = https://downloads.wordpress.org/theme/bravada.1.0.3.1.zip;
-          sha256 = "1q1g00pskr7z4mws4jvn1bxxd1licavzjikvpbg7y1zwh109rn1k";
+          url = https://downloads.wordpress.org/theme/bravada.1.0.7.1.zip;
+          sha256 = "0iq24r6by3mp0za41v5hjjlav4smbahjwg02h9x4rvs92h5qxg2m";
         };
         # We need unzip to build this package
         buildInputs = [ pkgs.unzip ];
@@ -93,8 +93,8 @@
         name = "kadence-theme";
         # Download the theme from the wordpress site
         src = pkgs.fetchurl {
-          url = https://downloads.wordpress.org/theme/kadence.1.0.11.zip;
-          sha256 = "11i2367dz2vsqddhwzwh9zlk075n44l07ra5s0jjssp2qya8m3x8";
+          url = https://downloads.wordpress.org/theme/kadence.1.1.22.zip;
+          sha256 = "18n3h0q8wzpgvzwj5vif37ni51vcq3clc6wxl80g2prqc2s0j8gq";
         };
         # We need unzip to build this package
         buildInputs = [ pkgs.unzip ];
@@ -106,8 +106,8 @@
         name = "popularfx-theme";
         # Download the theme from the wordpress site
         src = pkgs.fetchurl {
-          url = https://downloads.wordpress.org/theme/popularfx.1.1.9.zip;
-          sha256 = "1221jjs1fpbrm0m1baacxh7i14kjkds8rwxxc3qvj0jclyfnbfbg";
+          url = https://downloads.wordpress.org/theme/popularfx.1.2.3.zip;
+          sha256 = "17wjxr0kspsi7qdaj4by9frvd1bm8sj6h0rjncybnj7slq7dgqm7";
         };
         # We need unzip to build this package
         buildInputs = [ pkgs.unzip ];
@@ -119,8 +119,8 @@
         name = "colibri-theme";
         # Download the theme from the wordpress site
         src = pkgs.fetchurl {
-          url = https://downloads.wordpress.org/theme/colibri-wp.1.0.81.zip;
-          sha256 = "1sjvz63lhlrkqgwgng7z7k7jy6vjhw5wnpag9kwqy8z0ks8pbqsj";
+          url = https://downloads.wordpress.org/theme/colibri-wp.1.0.88.zip;
+          sha256 = "1bqbzy650mz0b76ij2d1mnzrk3fz5d1a83r5wmgmw7gqc1wfmlhb";
         };
         # We need unzip to build this package
         buildInputs = [ pkgs.unzip ];
@@ -132,8 +132,8 @@
         name = "skyline-theme";
         # Download the theme from the wordpress site
         src = pkgs.fetchurl {
-          url = https://downloads.wordpress.org/theme/skyline-wp.1.0.3.zip;
-          sha256 = "0q5gh777v745cs1y8z8vnqj43yh6vr7js1s6h0knq4klq387dd2i";
+          url = https://downloads.wordpress.org/theme/skyline-wp.1.0.6.zip;
+          sha256 = "0g7cqdzi0npihpa70wa7vqlvjv6fyqk0a7n8bd3cqjyj56i8xxwv";
         };
         # We need unzip to build this package
         buildInputs = [ pkgs.unzip ];
@@ -145,8 +145,8 @@
         name = "ashe-theme";
         # Download the theme from the wordpress site
         src = pkgs.fetchurl {
-          url = https://downloads.wordpress.org/theme/ashe.1.9.7.99.03.zip;
-          sha256 = "09r22iqalq1f6v8hd7gdw74017ani7k2v26a43fljyjfp0xc2y03";
+          url = https://downloads.wordpress.org/theme/ashe.2.199.zip;
+          sha256 = "0hf0m1k469mk0ivvjn4s7hrkggw6y1a6mjqk2m7pjdrlnap9785f";
         };
         # We need unzip to build this package
         buildInputs = [ pkgs.unzip ];
@@ -158,8 +158,8 @@
         name = "twenty-eleven-theme";
         # Download the theme from the wordpress site
         src = pkgs.fetchurl {
-          url = https://downloads.wordpress.org/theme/twentyeleven.3.6.zip;
-          sha256 = "04baww3sqpqq10nq0k4inv52s9xvdql4vwx8cadj9gbfy3rn445w";
+          url = https://downloads.wordpress.org/theme/twentyeleven.4.1.zip;
+          sha256 = "08pnjigq9iw3r20ja441ivsx3n557bcx829i5spfqdxjgljggnki";
         };
         # We need unzip to build this package
         buildInputs = [ pkgs.unzip ];
@@ -171,8 +171,8 @@
         name = "twenty-seventeen-theme";
         # Download the theme from the wordpress site
         src = pkgs.fetchurl {
-          url = https://downloads.wordpress.org/theme/twentyseventeen.2.8.zip;
-          sha256 = "1hqla7vm0c1fqn330va27d16mcfscrapxfy51byjpqylj6lh6yiw";
+          url = https://downloads.wordpress.org/theme/twentyseventeen.3.0.zip;
+          sha256 = "1lw5gvxzjzk5py475x2c0cnk8k8r574i0lhhxzhzqifz7vd4l352";
         };
         # We need unzip to build this package
         buildInputs = [ pkgs.unzip ];
