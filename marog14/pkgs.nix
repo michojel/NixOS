8ee0421bafa5f438b9ee349c6aa26823b21e0c3a
Michal Minář <mm@michojel.cz>
2022-08-16 14:19:09 +0200

    added marog14
    Signed-off-by: Michal Minář <mm@michojel.cz>

---
+++ marog14/pkgs.nix
@@ -0,0 +1,96 @@
+{ config, pkgs, nodejs, ... }:
+
+with config.nixpkgs;
+let
+  unstable = import <nixos-unstable> {
+    config = {
+      allowUnfree = true;
+    };
+  };
+in
+rec {
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = with pkgs; [
+    # CLI *****************************
+    duply
+    exfat
+    exfatprogs
+    iperf
+    qemu
+    samba
+    zfstools
+
+    # HW
+    inxi
+    radeontop
+
+    # devel
+    awscli
+    ansible
+    ansible-lint
+    asciidoc-full
+    awless
+    helm
+    ltrace
+    kind
+    krew
+    kubectl
+    kubernetes-helm
+    unstable.operator-sdk
+    controller-tools
+    kustomize
+    rpm
+    s3cmd
+    skopeo
+    texlive.combined.scheme-full
+    vagrant
+
+    # network
+    mobile-broadband-provider-info
+    modemmanager
+    libqmi
+    libreswan
+    usb-modeswitch
+    linuxPackages.usbip
+    # insecure
+    #tightvnc
+    gtk-vnc
+    gtk-vnc.bin
+    gtk-vnc.man
+    x11vnc
+    directvnc
+    x2vnc
+    virt-viewer
+    #kdeApplications.krdc
+    remmina
+
+    ssvnc
+
+    # GUI *****************************
+    razergenie
+    #opera
+    thunderbird
+    gnome3.vinagre
+    unetbootin
+    ventoy-bin
+
+    # chat
+    slack
+
+    virtmanager
+
+    # play
+    #unstable.wine
+    #unstable.winetricks
+
+    # video
+    #ffmpeg-sixel
+    pitivi
+
+    # browsers
+    google-chrome
+  ];
+}
+
+# ex: set et ts=2 sw=2 :
---
+++ marog14/zfs.nix
@@ -0,0 +1,24 @@
+{ config, lib, pkgs, ... }:
+
+{
+  boot = {
+    zfs = {
+      requestEncryptionCredentials = true;
+      devNodes = "/dev/";
+    };
+    supportedFilesystems = [ "zfs" ];
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+    kernelParams = [ "nohibernate" ];
+  };
+
+  services = {
+    zfs = {
+      autoScrub.enable = true;
+      autoScrub.pools = [ "rpool" ];
+      autoSnapshot.enable = true;
+    };
+  };
+}
