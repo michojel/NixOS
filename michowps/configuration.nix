57efa09b0cbc51dc2df56871772158699e95164f
Michal Minář <mm@michojel.cz>
2022-10-15 19:51:24 +0200

    michowps: manage ankisyncd data as a user
    Signed-off-by: Michal Minář <mm@michojel.cz>

--- michowps/configuration.nix
+++ michowps/configuration.nix
@@ -174,4 +174,8 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "22.05"; # Did you read the comment?
 
+  users.users."${config.local.username}" = {
+    extraGroups = pkgs.lib.mkAfter [ "ankisyncd" ];
+  };
+
 }
