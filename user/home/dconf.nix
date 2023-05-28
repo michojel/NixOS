32ecb07be754e6034aa94938782a9fd837a19e2d
Michal Minář <mm@michojel.cz>
2023-05-28 15:21:39 +0200

    dconf: gnome: lock screen with <Super><Shift>l
    Signed-off-by: Michal Minář <mm@michojel.cz>

--- user/home/dconf.nix
+++ user/home/dconf.nix
@@ -69,7 +69,7 @@ in
     "org/gnome/settings-daemon/plugins/media-keys" = {
       area-screenshot = [ "<Shift>Print" ];
       area-screenshot-clip = [ "<Primary><Shift>Print" ];
-      screensaver = [ "<Super><Control>l" ];
+      screensaver = [ "<Super><Shift>l" ];
       custom-keybindings = [
         "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
         "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
