1c7b87f5244bca9041342220583ecdcb83df8874
Michal Minář <mm@michojel.cz>
2023-06-06 21:53:11 +0200

    paperwm: bumped to v44.1.0
    Signed-off-by: Michal Minář <mm@michojel.cz>

--- overlays/paperwm.nix
+++ overlays/paperwm.nix
@@ -1,6 +1,6 @@
 self: super:
 let
-  version = "v44.0.1";
+  version = "v44.1.0";
 in
 {
   gnomeExtensions = super.gnomeExtensions // {
@@ -13,7 +13,7 @@ in
           owner = "paperwm";
           repo = "PaperWM";
           rev = version;
-          hash = "sha256-r0sCRMxhTzg91rlP2DOuPBPlLA/BGcr4YfOKhwwPJoU=";
+          hash = "sha256-J3yaq8dy3v9bZAoJu0ww305eSZvRzwP5SFkTf/hRPMg=";
         };
       }
     );
