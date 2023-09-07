5989e98f5bd9821d68145f785e5c66bab3fc9e96
Michal Minář <mm@michojel.cz>
2023-09-07 14:05:26 +0200

    vbox: more robust iptables forwarding
    Signed-off-by: Michal Minář <mm@michojel.cz>

--- common/virtualbox.nix
+++ common/virtualbox.nix
@@ -12,9 +12,11 @@
   networking.firewall.extraCommands = ''
     ip46tables -I INPUT 1 -i vboxnet+ -p tcp -m tcp --dport 2049 -j ACCEPT
 
-    iptables -A FORWARD -m conntrack -o vboxnet+ --ctstate ESTABLISHED,RELATED -j ACCEPT
-    iptables -A FORWARD -i vboxnet+ -o \!vboxnet+ -m conntrack --ctstate NEW -j ACCEPT
-    iptables -t nat -A POSTROUTING -o \!vboxnet+ --source 192.168.51.0/24 -j MASQUERADE
+    iptables        -A FORWARD                 -o   vboxnet+                    -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+    iptables        -A FORWARD     -i vboxnet+ -o \!vboxnet+ -s 192.168.51.0/24 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+    iptables        -A FORWARD     -i vboxnet+ -o \!vboxnet+ -s 192.168.51.0/24 -m conntrack --ctstate NEW                 -j ACCEPT
+    iptables        -A FORWARD     -i vboxnet+ -o   vboxnet+ -s 192.168.51.0/24 -m conntrack --ctstate NEW                 -j ACCEPT
+    iptables -t nat -A POSTROUTING             -o \!vboxnet+ -s 192.168.51.0/24                                            -j MASQUERADE
   '';
 
   networking.hosts = {
