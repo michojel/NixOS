1e86ea6b3f8277e7fd78b1694c38566ca4a7a4f9
Michal Minář <mm@michojel.cz>
2024-03-24 19:39:45 +0100

    wp-sites: groups plugin hash update
    Signed-off-by: Michal Minář <mm@michojel.cz>

--- michowps/wp-sites.nix
+++ michowps/wp-sites.nix
@@ -65,7 +65,7 @@ in
           forceLoginPlugin = wpArtifact "plugin" "force-login"
             "wp-force-login.5.6.3" "sha256-IImyQ174kxFkpERysoWujDcWw9bfVA8oID+DIHCRNzE=";
           groupsPlugin = wpArtifact "plugin" "groups"
-            "groups" "1qyvqwmc4kj5cwyymqi3zpjzs63z8z59cyyzpn7c2p0hb0i5kagk";
+            "groups" "sha256-crP/PEyq+72jgaaEoylTwIupnR1HqAF/c6EA+fgLrn0=";
           locoTranslatePlugin = wpArtifact "plugin" "loco-translate-plugin"
             "loco-translate.2.6.6" "sha256-qISTickGS1S1HyHp9P0a5KINsO49/TH+t4r5awLaY0w=";
           modulaPlugin = wpArtifact "plugin" "modula-plugin"
