046fe03f2d704765e6d053b27d5c8e740f18b25a
Michal Minář <mm@michojel.cz>
2024-08-29 20:56:02 +0200

    switch to the stock kernel again
    Signed-off-by: Michal Minář <mm@michojel.cz>

--- minap15/hardware-configuration.nix
+++ minap15/hardware-configuration.nix
@@ -32,7 +32,7 @@
   ];
   # as of 6.6.45, audio is broken
   # viz https://www.linuxquestions.org/questions/showthread.php?p=6517834#post6517834
-  boot.kernelPackages = pkgs.linuxPackages_6_1;
+  # boot.kernelPackages = pkgs.linuxPackages_6_1;
 
   fileSystems."/" =
     {
